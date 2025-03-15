88146303fb8f441d2508e2fd675070b800a14a15
Hansel-Commit <hanselzamliz@gmail.com>
2025-03-15 14:09:33 -0500

    proyecto actualizado del challenger

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo'); 
+    let nombreDeAmigo = input.value.trim(); // 
+
+    if (nombreDeAmigo !== "") { 
+        amigos.push(nombreDeAmigo); 
+        actualizarLista(); 
+
+        input.value = ""; 
+        input.focus(); 
+    }
+}
+document.getElementById("amigo").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        agregarAmigo();
+    }
+});
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML = ""; 
+
+    amigos.forEach(amigo => {
+        let li = document.createElement("li"); 
+        li.textContent = amigo; 
+        lista.appendChild(li); 
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        document.getElementById("resultado").innerHTML = "<li>No hay amigos en la lista para sortear.</li>";
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `<li> (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧Tu amigo secreto es: <strong>${amigoSecreto}</strong> (ﾉ^_^)ﾉ</li>`;
+}
